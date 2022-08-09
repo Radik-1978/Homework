da144b75e250153997d68bcbe9c85e42adccd2b5
Radik-1978 <109205944+Radik-1978@users.noreply.github.com>
2022-08-09 08:00:18 +0300

    задача10

---
+++ Task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using static System.Console;
+Clear();
+Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
---
+++ Task_10/Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https:Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+using static System.Console;
+Write("Введите число: ");
+int Number = Convert.ToInt32(ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  WriteLine("третья цифра: " + NumberText[2]);
+}
+else {
+  WriteLine("третьей цифры нет");
+}
