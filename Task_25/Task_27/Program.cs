6507fb9ed15f5d82e3a54faf939f089a3fd63618
Radik-1978 <109205944+Radik-1978@users.noreply.github.com>
2022-08-18 09:17:42 +0300

    задача 29

---
+++ Task_25/Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+using static System.Console;
+Clear();
+Write("Введите число N: ");
+int numberN = Convert.ToInt32(ReadLine());
+int sum = SumNumber(numberN);
+Console.WriteLine($"Сумма цифр в числе = {sum}");
+
+
+
+
+int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+
---
+++ Task_29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+ using static System.Console;
+Clear();
+
+int[] Mass = GetmyArray(8);
+WriteLine($"[{String.Join(", ", Mass)}]");
+
+
+int[] GetmyArray(int N)
+{
+int[] myarray = new int[N];
+for (int i=0; i<N; i++)
+{
+    myarray[i] = new Random().Next(0,33);
+}
+return myarray;
+}   
+
+
+
+
+
+
+ 
+            
+    
+
+
+
+
